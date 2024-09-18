e2c871ff61db98fe1edbd475ede892b2b5eaeb16
viktorefimovich <viktorefimovich@bk.ru>
2024-09-18 11:14:25 +0700

    Исправлено

--- src/abc_classes.py
+++ src/abc_classes.py
@@ -6,7 +6,7 @@ class APIParser(ABC):
     """Абстрактный класс для работы с API сайта"""
 
     @abstractmethod
-    def get_vacancies(self, keyword: str, page: int = 0, per_page: int = 100) -> Any:
+    def get_vacancies(self, keyword: str) -> Any:
         """Абстрактный метод получения вакансий с API сайта по ключевому слову"""
 
         pass
