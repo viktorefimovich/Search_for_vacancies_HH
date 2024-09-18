88a8aa665f4d6fe98ae1b86093e791ab3c173988
viktorefimovich <viktorefimovich@bk.ru>
2024-09-18 09:00:47 +0700

    Исправленно(типизация)

--- src/abc_classes.py
+++ src/abc_classes.py
@@ -1,11 +1,12 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
 
 class APIParser(ABC):
     """Абстрактный класс для работы с API сайта"""
 
     @abstractmethod
-    def get_vacancies(self, keyword: str, page: int = 0, per_page: int = 100):
+    def get_vacancies(self, keyword: str, page: int = 0, per_page: int = 100) -> Any:
         """Абстрактный метод получения вакансий с API сайта по ключевому слову"""
 
         pass
@@ -15,19 +16,19 @@ class FilesWork(ABC):
     """Абстрактный класс для работы с файлами"""
 
     @abstractmethod
-    def get_vacancy(self):
+    def get_vacancy(self) -> Any:
         """Абстрактный метод получения данных из файла"""
 
         ...
 
     @abstractmethod
-    def add_vacancy(self):
+    def add_vacancy(self) -> Any:
         """Абстрактный метолд добавления данных в файл"""
 
         ...
 
     @abstractmethod
-    def delete_vacancy(self):
+    def delete_vacancy(self) -> Any:
         """Абстрактный метолд удаления данных из файла"""
 
         ...
