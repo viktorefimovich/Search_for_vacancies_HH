3b65218f0fa40fb6d9367a219feaec24eb784017
viktorefimovich <viktorefimovich@bk.ru>
2024-09-19 14:59:52 +0700

    Добавлен класс JSONWorker для работы с JSON файлами Добавлены методы: для получения данных из файла, для сохранения в файл, для добавления в файл, для удаления данных из файла

---
+++ src/json_worker.py
@@ -0,0 +1,55 @@
+import json
+from pathlib import Path
+from typing import Any
+
+from src.abc_classes import FilesWork
+from src.vacancy import Vacancy
+
+ROOTPATH = Path(__file__).resolve().parent.parent
+
+
+class JSONWorker(FilesWork):
+    """Класс для работы с json файлами"""
+
+    __file_name: str
+    __path_to_file: Path
+
+    def __init__(self, file_name: str = "vacancies.json") -> None:
+        self.__file_name = self.__check_and_get_file_name(file_name)
+        self.__path_to_file = Path(ROOTPATH, f"{self.__file_name}")
+
+    def get_from_file(self) -> Any:
+        """Метод для получения данных из файла"""
+
+        with open(self.__path_to_file, "r") as file:
+            return json.load(file)
+
+    def save_to_file(self, vacancies: list[dict]) -> None:
+        """Метод для сохранения в файл списка вакансий"""
+
+        with open(self.__path_to_file, "w") as file:
+            json.dump(vacancies, file, indent=4, ensure_ascii=False)
+
+    def add_to_file(self, vacancies: list[dict]) -> None:
+        """Метод для добавления в файл вакансий без дублирования"""
+
+        vacancies_in_file = self.get_from_file()
+        ids_vacancies = Vacancy.get_list_id_vacancies(vacancies)
+        ids_vacancies_in_file = Vacancy.get_list_id_vacancies(vacancies_in_file)
+        add_id_vacancies = list(set(ids_vacancies).difference(set(ids_vacancies_in_file)))
+        for vacancy_id in add_id_vacancies:
+            i = ids_vacancies.index(vacancy_id)
+            vacancies_in_file.append(vacancies[i])
+
+        self.save_to_file(vacancies_in_file)
+
+    def delete_from_file(self) -> None:
+        """Общий функционал для удаления данных из файла"""
+
+        self.save_to_file([])
+
+    @staticmethod
+    def __check_and_get_file_name(file_name: str) -> str:
+        if file_name[-5:] != ".json":
+            return f"{file_name}.json"
+        return file_name
