cb45c4d3d7a62970830bd03846997bbaa2e53750
viktorefimovich <viktorefimovich@bk.ru>
2024-09-26 12:38:00 +0700

    Исправлено

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,3 +26,117 @@ def vacancy_data():
         "requirement": "Python, Django",
         "responsibility": "Develop backend services"
     }
+
+
+@pytest.fixture
+def vacancy_data2():
+    return {
+        "id": 2,
+        "name": "Software Engineer",
+        "location": "Москва",
+        "salary_from": 120000,
+        "salary_to": 150000,
+        "salary_string": "От 100000 до 150000",
+        "published_at": "2024-01-01",
+        "url": "http://example.com",
+        "name_employer": "ExampleCompany",
+        "experience": "3-5 лет",
+        "requirement": "Python, Django",
+        "responsibility": "Develop backend services"
+    }
+
+
+@pytest.fixture
+def vacancy_data3():
+    return {
+        "id": 3,
+        "name": "Software Engineer",
+        "location": "Москва",
+        "salary_from": 120000,
+        "salary_to": 150000,
+        "salary_string": "От 100000 до 150000",
+        "published_at": "2024-01-01",
+        "url": "http://example.com",
+        "name_employer": "ExampleCompany",
+        "experience": "3-5 лет",
+        "requirement": "Python, Django",
+        "responsibility": "Develop backend services"
+    }
+
+
+@pytest.fixture
+def vacancy_data_none():
+    return {
+        "id": 2,
+        "name": None,
+        "location": None,
+        "salary_from": None,
+        "salary_to": None,
+        "salary_string": None,
+        "published_at": None,
+        "url": None,
+        "name_employer": None,
+        "experience": None,
+        "requirement": None,
+        "responsibility": None
+    }
+
+
+@pytest.fixture
+def partial_vacancy_data():
+    return {
+        "id": 1,
+        "name": "Software Engineer",
+        "location": "Moscow",
+        "salary_string": "Зарплата не указана",
+        "published_at": "2023-01-01",
+        "url": "http://example.com",
+        "name_employer": "ExampleCompany",
+    }
+
+
+@pytest.fixture
+def empty_vacancy_data():
+    return {}
+
+
+@pytest.fixture
+def vacancies_from_hh():
+    return [
+        {
+            "id": "12345",
+            "name": "Python Developer",
+            "area": {"name": "Moscow"},
+            "salary": {
+                "from": 100000,
+                "to": 150000,
+                "currency": "RUR"
+            },
+            "published_at": "2023-09-01T00:00:00",
+            "alternate_url": "https://hh.ru/vacancy/12345",
+            "employer": {"name": "Tech Company"},
+            "experience": {"name": "3-5 лет"},
+            "snippet": {
+                "requirement": "Опыт работы с Python, Django",
+                "responsibility": "Разработка веб-приложений"
+            }
+        },
+        {
+            "id": "67890",
+            "name": "Data Scientist",
+            "area": {"name": "Saint Petersburg"},
+            "salary": {
+                "from": 120000,
+                "to": 180000,
+                "currency": "RUR"
+            },
+            "published_at": "2023-09-02T00:00:00",
+            "alternate_url": "https://hh.ru/vacancy/67890",
+            "employer": {"name": "Data Company"},
+            "experience": {"name": "1-3 года"},
+            "snippet": {
+                "requirement": "Опыт работы с машинным обучением",
+                "responsibility": "Анализ данных и построение моделей"
+            }
+        }
+    ]
