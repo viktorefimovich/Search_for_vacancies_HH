88e305674d606ae90d398e40bc2dd5d9a148eb2f
viktorefimovich <viktorefimovich@bk.ru>
2024-09-26 11:20:28 +0700

    Исправлено

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,21 @@ def json_worker():
     """Фикстура для создания объекта JSONWorker"""
 
     return JSONWorker("test_file.json")
+
+
+@pytest.fixture
+def vacancy_data():
+    return {
+        "id": 1,
+        "name": "Software Engineer",
+        "location": "Москва",
+        "salary_from": 100000,
+        "salary_to": 150000,
+        "salary_string": "От 100000 до 150000",
+        "published_at": "2024-01-01",
+        "url": "http://example.com",
+        "name_employer": "ExampleCompany",
+        "experience": "3-5 лет",
+        "requirement": "Python, Django",
+        "responsibility": "Develop backend services"
+    }
